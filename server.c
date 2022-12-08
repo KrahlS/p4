34e6c064df37313ce8eef0dbcb136e4f17ab942a
mshoval <shoval@wisc.edu>
2022-12-07 18:40:16 -0600

    libmfs.so and server.c

---
+++ server.c
@@ -0,0 +1,10 @@
+#include mkfs.c
+#include libmfs.so
+
+int main(int argc, char *argv[]) {
+    // portnum = argv[1]
+    // mmap filesystem image (argv[2])
+    while(1) {
+        // scan input and respond
+    }
+}
